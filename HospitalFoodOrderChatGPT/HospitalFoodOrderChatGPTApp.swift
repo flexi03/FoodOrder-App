bdb188511402e036dcf690dc834320f215262002
Felix Kircher <felixkircher.fk@icloud.com>
2023-09-16 00:12:22 +0200

    Initial Commit

---
+++ HospitalFoodOrderChatGPT/HospitalFoodOrderChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HospitalFoodOrderChatGPTApp.swift
+//  HospitalFoodOrderChatGPT
+//
+//  Created by Felix Kircher on 16.09.23.
+//
+
+import SwiftUI
+
+@main
+struct HospitalFoodOrderChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
