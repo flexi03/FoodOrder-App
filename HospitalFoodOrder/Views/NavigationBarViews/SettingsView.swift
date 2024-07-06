81e0e5ac75d8c63b9bd98b6774d3b5b812a00415
flexi03 <felixkircher.fk@icloud.com>
2024-07-06 21:57:50 +0200

    Version notes in Settings

--- HospitalFoodOrder/Views/NavigationBarViews/SettingsView.swift
+++ HospitalFoodOrder/Views/NavigationBarViews/SettingsView.swift
@@ -14,38 +14,48 @@ struct SettingsView: View {
     @AppStorage("showWorkingTime") var showWorkingTime: Bool = true
     
     var body: some View {
-        Form {
-            Section("App Farbe") {
-                Picker("Color", selection: $colorScheme.mode) {
-                    Text("Dunkel").tag("Dunkel")
-                    Text("Hell").tag("Hell")
+        VStack {
+            Form {
+                Section("App Farbe") {
+                    Picker("Color", selection: $colorScheme.mode) {
+                        Text("Dunkel").tag("Dunkel")
+                        Text("Hell").tag("Hell")
+                    }
+                    .pickerStyle(.segmented)
+                    .frame(height: 50)
                 }
-                .pickerStyle(.segmented)
-                .frame(height: 50)
+                Section(header: Text("Optionen")) {
+                    Toggle(isOn: $settings.coffeeSelected) {
+                        Text("Kaffee auswählbar")
+                    }
+                    Toggle(isOn: $settings.showRestrictions) {
+                        Text("Einschränkungen anzeigen")
+                    }
+                    NavigationLink("Auswahl Optionen", destination: OptionView(settings: Settings()))
+                }
+                
+                Section(header: Text("Entwickler")) {
+                    //                Toggle(isOn: $showWorkingTime, label: {
+                    //                    Text("Arbeitszeiterfassung")
+                    //                })
+                    NavigationLink("Entwickler", destination: DeveloperSettingsView())
+                }
+                
             }
-            Section(header: Text("Optionen")) {
-                Toggle(isOn: $settings.coffeeSelected) {
-                    Text("Kaffee auswählbar")
-                }
-                Toggle(isOn: $settings.showRestrictions) {
-                    Text("Einschränkungen anzeigen")
-                }
-                NavigationLink("Auswahl Optionen", destination: OptionView(settings: Settings()))
-            }
+            .navigationTitle("Einstellungen")
+            .navigationBarItems(trailing: NavigationLink(destination: FeedbackView()) {
+                Image(systemName: "bubble.left.and.exclamationmark.bubble.right")
+                    .fontWeight(.bold)
+            })
             
-            Section(header: Text("Entwickler")) {
-//                Toggle(isOn: $showWorkingTime, label: {
-//                    Text("Arbeitszeiterfassung")
-//                })
-                NavigationLink("Entwickler", destination: DeveloperSettingsView())
-            }
+            // Version number and Build
+            let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+            let buildNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion") as! String
+            Text("Version \(version) Build \(buildNumber)")
+                .font(.footnote)
+                .frame(height: 120, alignment: .top)
             
         }
-        .navigationTitle("Einstellungen")
-        .navigationBarItems(trailing: NavigationLink(destination: FeedbackView()) {
-            Image(systemName: "bubble.left.and.exclamationmark.bubble.right")
-                .fontWeight(.bold)
-        })
     }
 }
 
